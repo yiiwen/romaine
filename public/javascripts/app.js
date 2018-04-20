6eeee1cd9f4a4f21aeaa4972dda96a6a4726370f
weiki <wen18826478504@163.com>
2018-04-20 15:14:59 +0800

    首页模板

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,26 +1,65 @@
 $(document).ready(function(){
+
+    //新闻模块
+    initNewTab();
+    newTabSwitch();
+});
+
+//新闻标签页初始化
+function initNewTab() {
+    var width = $(".table-page").css("width");
+    var height = $(".table-page").css("height");
+    var size = $(".tab").length;
+    width = parseInt(width.substring(0,width.length -2));
+    $(".tab-content").css("width",size * width +"px");
+    $(".tab-content").css("height",height);
+    $(".tab").css('width',width).css("height",height);
+}
+//新闻标签页切换
+function newTabSwitch() {
+
     $('.newBtn').click(function(){
         $(".newBtn").removeClass("active");
         $(this).addClass("active");
+
         var width = $(".table-page").css("width");
-        width = parseInt(width.substring(0,width.length -2)) -2;
-        var time = setInterval(function(){
-            $(".tab").each(function(){
-                var left = $(this).css("left");
-                left = parseInt(left.substring(0,left.length-2));
-                left--;
-                if (Math.abs(left) % width == 0){
-                    clearInterval(time);
-                    //将第一个转到最后
-                    var length = $(".tab").length;
-                    $(".table-page .active").css('left',(length-1) * (width-2) +"px");
-                    if ($(this).css('left') == '1px'){
-                        $(this).removeClass("active");
-                        $(this).addClass('active');
-                    }
+        var index = $(this).index();
+        var pointX = $(".tab-content").css("left")
+        width = parseInt(width.substring(0,width.length -2));
+        pointX = parseInt(pointX.substring(0,pointX.length -2));
+        var shift = -(index * width);
+        var speed = 1;
+        
+        if (shift ==pointX) {
+            return true;   
+        } else if (shift > pointX) {
+            var time = setInterval(function(){
+                if (Math.abs(shift - pointX) < speed ) {
+                    speed =1;
+                } else {
+                    speed ++;
                 }
-                $(this).css('left',left +'px');
-            });
-        },10);
+                if (shift > pointX) {
+                    pointX +=speed;
+                    $(".tab-content").css("left",pointX+"px");
+                } else {
+                    clearInterval(time);
+                }
+            },10);    
+        } else {
+            var time = setInterval(function(){
+                if (Math.abs(shift - pointX) < speed ) {
+                    speed =1;
+                } else {
+                    speed ++;
+                }
+                if (shift < pointX) {
+                    pointX -=speed;
+                    $(".tab-content").css("left",pointX +"px");
+                } else {
+                    clearInterval(time);
+                }
+            },5);
+        }
     });
-});
+}
