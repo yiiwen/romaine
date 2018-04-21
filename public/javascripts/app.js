398d232959568b51d68ea9e538dc1208434e7e6e
weiki <wen18826478504@163.com>
2018-04-21 15:23:27 +0800

    数字变动

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -12,7 +12,56 @@ $(document).ready(function(){
         $("html,body").animate({scrollTop:0},300);
     });
 
+    scrollNum();
+
 });
+
+//数字滚动
+function scrollNum(){
+    
+    var num3 = 0;
+    var time3 = setInterval(function(){
+        $(".number").eq(3).text(num3+=10);
+        if (num3 == parseInt($(".number").eq(3).attr("max"))) {
+            $(".number").eq(3).text(num3 +"+");
+            $(".number-abs").eq(3).show(100);
+            clearInterval(time3);
+        }
+    },10);
+    
+    var num2 = 0;
+    var time2 = setInterval(function(){
+        $(".number").eq(2).text(num2+=2);
+        if (num2 == parseInt($(".number").eq(2).attr("max"))) {
+            $(".number").eq(2).text(num2 +"+");
+            $(".number-abs").eq(2).show(100);
+            clearInterval(time2);
+        }
+    },10);
+
+    var num1 = 0;
+    var time1 = setInterval(function(){
+        $(".number").eq(1).text(num1++);
+        if (num1 == parseInt($(".number").eq(1).attr("max"))) {
+            $(".number").eq(1).text(num1 +"+");
+            $(".number-abs").eq(1).show(100);
+            clearInterval(time1);
+        }
+    },50);
+
+    var num = 0;
+    var time = setInterval(function(){
+        $(".number").eq(0).text(num++);
+        if (num == parseInt($(".number").eq(0).attr("max"))) {
+            $(".number").eq(0).text(num +"+");
+            $(".number-abs").eq(0).show(100);
+            clearInterval(time);
+        }
+    },50);
+
+    
+    
+}
 
 //新闻标签页初始化
 function initNewTab() {
