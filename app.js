70561e9eb73f0e0dacc3a8f903b0adb3097bb33f
yiiwen <wen18826478504@163.com>
2018-04-25 23:06:37 +0800

    案例

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var newsRouter = require('./routes/news');
 var infoRouter = require('./routes/info');
+var caseRouter = require('./routes/cases');
 
 var app = express();
 app.locals.title = '油麦菜信息科技有限公司';
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/news',newsRouter);
 app.use('/info',infoRouter);
+app.use('/cases',caseRouter);
 
 app.use(function(req, res, next) {
   res.render("404",{title:"广州油麦菜信息科技有限公司"});
